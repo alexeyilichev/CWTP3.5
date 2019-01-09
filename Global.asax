84d7f24c51afb30b67073a9655ea4232a5cdbc85
Alexey Ilichev <34123404+alexeyilichev@users.noreply.github.com>
2019-01-09 08:00:37 +0300

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CW.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace CW
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        protected void Application_Start()
+        {
+            logger.Info("Application Start");
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
